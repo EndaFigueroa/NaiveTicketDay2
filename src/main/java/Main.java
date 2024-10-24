cbabfa3a3fd89c83a3e108f0ee48b34bc0a5f051
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-10-23 23:26:26 -0400

    Did the code-specific stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Naive Ticket running.");
-    }
+        TicketMachine pizza = new TicketMachine(300);
+        System.out.println(pizza.getPrice());
+        pizza.insertMoney(200);
+        System.out.println(pizza.getBalance());
+        pizza.printTicket();
+        System.out.println(pizza.getBalance());
+        pizza.insertMoney(1999);
+        System.out.println(pizza.getBalance());
 
-    // @Test
-    // void addition() {
-    //     assertEquals(2, 1 + 1);
-    // }
+        TicketMachine gir = new TicketMachine(3);
+        gir.insertMoney(3);
+        gir.printTicket();
+        // @Test
+        // void addition() {
+        //     assertEquals(2, 1 + 1);
+        // }
+    }
 }
